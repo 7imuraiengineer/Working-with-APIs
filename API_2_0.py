40de13276ccfbf977515e59d54255a53a8247792
timurai <233756151+7imuraiengineer@users.noreply.github.com>
2025-11-09 22:42:01 +0900

    Update weather API code and add gitignore
    - Fixed weather forecast display function
    - Added .gitignore to exclude Python cache files

--- API_2_0.py
+++ API_2_0.py
@@ -1,25 +1,16 @@
 import requests
-from datetime import datetime, UTC, timedelta
+from datetime import datetime, timedelta
 # Make the API request
 
-url = "https://api.openweathermap.org/data/2.5/weather"
 
-parametres = {
-    'q': 'Tashkent',
-    'appid': '7c43be754b25a6c3c095335ccb16b232',
-    'units': 'metric'
-    }
-
-response = requests.get(url, params=parametres)
-data = response.json()
-
-time = data['sys']['sunrise']
-
-global_time = datetime.fromtimestamp(time)
-local_time = global_time + timedelta(hours=-5)
-time = local_time.strftime('%H:%M')
-
-print(time)
-
-
-
+def quyosh_vaqti(url, parametres):
+    response = requests.get(url, params=parametres)
+    data = response.json()
+    kunchb = ["sunrise", "sunset"]
+    times = {i: data['sys'][i] for i in kunchb}
+    for i, time in times.items():
+        global_time = datetime.fromtimestamp(time)
+        local_time = global_time + timedelta(hours=-5)
+        time = local_time.strftime('%H:%M')
+        times[i] = time
+    return f"Quyosh chiqishi vaqti: {times['sunrise']}, Quyosh botishi vaqti: {times['sunset']}"
